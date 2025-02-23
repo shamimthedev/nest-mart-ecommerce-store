1952bd5d0b438188dd7579f2921020857c957cd9
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-02-23 19:47:25 +0600

    (feat) Deals of the Day section added

--- src/components/Deals.jsx
+++ src/components/Deals.jsx
@@ -1,12 +1,7 @@
-import Product01 from '/product-01.png'
-import Product012 from '/product-1-2.jpg'
 import Rating from '@mui/material/Rating';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import CompareArrowsOutlinedIcon from '@mui/icons-material/CompareArrowsOutlined';
-import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 
-const Product = () => {
+const Deals = ({title, img, author, oldPrice, newPrice, rating}) => {
 
     // const productsData = [
     //     {
@@ -123,48 +118,31 @@ const Product = () => {
 
     // Detect Scroll to Add Background and Shadow
 
-
     return (
-        <div className="w-[300px] rounded-[15px] p-[25px] border border-[#ECECEC] transition-all ease-in-out duration-200 hover:border-green-200 hover:shadow-md relative group">
-            <div className="absolute top-0 left-0 bg-[#F74B81] rounded-tl-[15px] rounded-br-[15px]">
-                <span className="text-white font-lato text-xs px-5 py-2">Hot</span>
+        <div className="w-[378px] h-[462px] relative group">
+            <div className="w-full">
+                <img src={img} alt={title} className='w-full object-cover' />
             </div>
-            <div className="">
-                <div className="overflow-hidden w-full h-[246px] relative group">
-                    <img src={Product01} alt="" className='absolute w-full h-full object-cover top-0 left-0 z-1' />
-                    <img src={Product012} alt="" className='absolute w-full h-full object-cover top-0 left-0 transition-transform ease-in-out duration-500 group-hover:z-2 group-hover:scale-105' />
+            <div className="absolute left-[26px] right-[26px] bottom-[-10px] rounded-[10px] bg-white shadow-md w-[325px] px-[30px] py-6 transition-all ease-in-out duration-300 group-hover:translate-y-[-6px]">
+                <div className="mt-[5px] mb-6 flex flex-col gap-y-[10px]">
+                    <h3 className='leading-[20px] font-bold'>{title}</h3>
+                    <div className="flex items-center gap-x-2">
+                        <Rating className='text-sm' name="half-rating" defaultValue={2.5} precision={0.5} />
+                        <span className='text-[#B6B6B6] text-sm leading-6 font-lato'>({rating})</span>
+                    </div>
+                    <span className='text-[#B6B6B6] text-sm leading-6 font-lato'>By <span className='text-greeny'>{author}</span></span>
                 </div>
-
-            </div>
-            <div className="mt-[5px] mb-6 flex flex-col gap-y-[10px]">
-                <span className='text-[#ADADAD] text-xs font-lato leading-6'>Snack</span>
-                <h3 className='leading-[20px] font-bold'>Seeds of Change Organic Quinoa, Brown, & Red Rice</h3>
-                <div className="flex items-center gap-x-2">
-                    <Rating className='text-sm' name="half-rating" defaultValue={2.5} precision={0.5} />
-                    <span className='text-[#B6B6B6] text-sm leading-6 font-lato'>(2.5)</span>
+                <div className="flex justify-between items-center font-bold leading-6">
+                    <span className='mr-[10px] text-2xl text-greeny '>{oldPrice}</span>
+                    <span className='mr-auto text-sm text-[#ADADAD] line-through'>{newPrice}</span>
+                    <button className="flex items-center gap-x-[5px] rounded-sm bg-[#DEF9EC] text-greeny font-lato font-bold text-sm leading-6 px-5 py-[10px] cursor-pointer transition-colors hover:bg-greeny hover:text-white">
+                        <ShoppingCartOutlinedIcon />
+                        Add
+                    </button>
                 </div>
-                <span className='text-[#B6B6B6] text-sm leading-6 font-lato'>By <span className='text-greeny'>Stouffer</span></span>
-            </div>
-            <div className="flex justify-between items-center font-bold leading-6">
-                <span className='mr-[10px] text-2xl text-greeny '>$52.85</span>
-                <span className='mr-auto text-sm text-[#ADADAD] line-through'>$55.8</span>
-                <button className="flex items-center gap-x-[5px] rounded-sm bg-[#DEF9EC] text-greeny font-lato font-bold text-sm leading-6 px-5 py-[10px] cursor-pointer transition-colors hover:bg-greeny hover:text-white">
-                    <ShoppingCartOutlinedIcon />
-                    Add
-                </button>
-            </div>
-            <div className="absolute top-[-120px] left-0 w-full h-full flex items-center justify-center opacity-0 transition-all ease-in-out duration-300 group-hover:opacity-100 group-hover:z-10">
-                <ul className='flex bg-white text-greeny rounded-[6px] border border-[#BCE3C9]'>
-                    <li className='px-[10px] py-[8px] text-sm hover:text-[#fd7e14] transition-colors duration-200 cursor-pointer wrapper'><FavoriteBorderOutlinedIcon className='w-3' />
-                        <span className='tooltip'>Add to Wishlist</span></li>
-                    <li className='px-[10px] py-[8px] text-sm hover:text-[#fd7e14] transition-colors duration-200 cursor-pointer wrapper border-x border-[#BCE3C9]'><CompareArrowsOutlinedIcon className='w-3' />
-                        <span className='tooltip'>Compare</span></li>
-                    <li className='px-[10px] py-[8px] text-sm hover:text-[#fd7e14] transition-colors duration-200 cursor-pointer wrapper'><VisibilityOutlinedIcon className='w-3' />
-                        <span className='tooltip'>Quickview</span></li>
-                </ul>
             </div>
         </div>
     )
 }
 
-export default Product
+export default Deals
--- src/components/DealsOfTheDay.jsx
+++ src/components/DealsOfTheDay.jsx
@@ -1,10 +1,69 @@
-
+import Deals from "./Deals";
+import Deals01 from '/deals-01.png'
+import Deals02 from '/deals-02.png'
+import Deals03 from '/deals-03.png'
+import Deals04 from '/deals-04.png'
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const DealsOfTheDay = () => {
+
+  const productsData = [
+    {
+      id: 1,
+      img: Deals01,
+      title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+      rating: '2.5',
+      author: 'Stouffer',
+      oldPrice: '$52.85',
+      newPrice: '$55.8',
+    },
+    {
+      id: 2,
+      img: Deals02,
+      title: 'Perdue Simply Smart Organics Gluten Free',
+      rating: '3.5',
+      author: 'NestFood',
+      oldPrice: '$28.85',
+      newPrice: '$32.8',
+    },
+    {
+      id: 3,
+      img: Deals03,
+      title: "Signature Wood-Fired Mushroom and Caramelized",
+      rating: '4.0',
+      author: 'StarKist',
+      oldPrice: '$48.85',
+      newPrice: '$52.8',
+    },
+    {
+      id: 3,
+      img: Deals04,
+      title: "Simply Lemonade with Raspberry Juice",
+      rating: '4.0',
+      author: 'StarKist',
+      oldPrice: '$48.85',
+      newPrice: '$52.8',
+    },
+  ];
   return (
     <>
-
-
+      <section className="mt-[55px]" id="dealsOfTheDay">
+        <div className="max-w-[1610px] mx-auto">
+          <div className="flex items-baseline justify-between mb-12">
+            <h2 className="font-bold text-[32px] leading-[38px]">Deals Of The Day</h2>
+            <span className="flex gap-x-1 items-center font-semibold">
+              All deals <MdKeyboardArrowRight />
+            </span>
+          </div>
+          <div className="flex gap-x-6">
+            {productsData.map((product) => (
+              <div key={product.id} className="w-full h-full pr-3">
+                <Deals title={product.title} img={product.img} author={product.author} oldPrice={product.oldPrice} newPrice={product.newPrice} rating={product.rating}/>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   )
 }
