4690d2832f91427d473d62272ecbdd68aca64ee6
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-02-24 21:28:54 +0600

    (feat) Shop page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router"
 import About from './pages/About';
 import Home from './pages/Home';
 import RootLayout from "./layout/RootLayout";
+import Shop from "./pages/Shop";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="shop" element={<Shop />} />
         </Route>
       </Routes>
     </>
