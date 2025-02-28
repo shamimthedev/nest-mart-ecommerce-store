dff2e4a95b5c7cf0c1ad6f042d8aa4c14ff08094
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-02-28 23:38:32 +0600

    (feat) About Page updated

--- src/components/AboutFeatureCard.jsx
+++ src/components/AboutFeatureCard.jsx
@@ -1,14 +1,15 @@
-import AboutFeature01 from '/about-feature-01.svg'
 
-const AboutFeatureCard = () => {
+
+const AboutFeatureCard = ({feature}) => {
+   const {title, img} = feature
     return (
-        <div className="w-[423px] rounded-[15px] border border-[#ECECEC] hover:border-green-100 hover:shadow-md transition-all grid place-items-center gap-y-8 px-[34px] py-[51px]">
-            <img src={AboutFeature01} alt="" className='w-[100px] h-[100px] object-cover' />
-            <h3>Best Prices & Offers</h3>
-            <p>There are many variations of passages of Lorem
+        <div className="w-[423px] rounded-[15px] border border-[#ECECEC] hover:border-green-100 hover:shadow-md transition-all grid place-items-center gap-y-[30px] px-[34px] py-[51px] leading-6 group">
+            <img src={img} alt="" className='w-[100px] h-[100px] object-cover mb-[7px] transition-all ease-in-out duration-200 group-hover:translate-y-[-4px]' />
+            <h3 className='font-bold'>{title}</h3>
+            <p className='mb-1 font-lato text-center text-[#7E7E7E]'>There are many variations of passages of Lorem
                 Ipsum available, but the majority have suffered
                 alteration in some form</p>
-            <button>Read more</button>
+            <button className='border-none font-lato outline-none text-greeny'>Read more</button>
         </div>
     )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,6 +9,14 @@ import AboutSlider02 from '/about-slider-02.png'
 import AboutSlider03 from '/about-slider-03.png'
 import Divider from '/about-divider.png'
 import AboutFeatureCard from '../components/AboutFeatureCard';
+import AboutFeature01 from '/about-feature-01.svg'
+import AboutFeature02 from '/about-feature-02.svg'
+import AboutFeature03 from '/about-feature-03.svg'
+import AboutFeature04 from '/about-feature-04.svg'
+import AboutFeature05 from '/about-feature-05.svg'
+import AboutFeature06 from '/about-feature-06.svg'
+import AboutPerformance from '/about-performance.png'
+import AboutCounter from '/about-counter-background.png'
 
 const About = () => {
   var settings = {
@@ -59,6 +67,39 @@ const About = () => {
     },
   ];
 
+  const featureData = [
+    {
+      id: 1,
+      title: 'Best Prices & Offers',
+      img: AboutFeature01
+    },
+    {
+      id: 2,
+      title: 'Wide Assortment',
+      img: AboutFeature02
+    },
+    {
+      id: 3,
+      title: 'Free Delivery',
+      img: AboutFeature03
+    },
+    {
+      id: 4,
+      title: 'Easy Returns',
+      img: AboutFeature04
+    },
+    {
+      id: 5,
+      title: '100% Satisfaction',
+      img: AboutFeature05
+    },
+    {
+      id: 6,
+      title: 'Great Daily Deal',
+      img: AboutFeature06
+    },
+  ]
+
   return (
     <>
       {/* Welcome Section */}
@@ -100,15 +141,89 @@ const About = () => {
         <div className="max-w-[1341px] mx-auto mt-[57px]">
           <div className="relative mb-10">
             <h2 className='font-bold text-[40px] text-center leading-12 pb-6'>What We Provide?</h2>
-            <img src={Divider} alt="" className='absolute bottom-0 left-1/2 -translate-x-1/2'/>
+            <img src={Divider} alt="" className='absolute bottom-0 left-1/2 -translate-x-1/2' />
           </div>
-          <div className="flex flex-wrap">
-            <AboutFeatureCard/>
-            <AboutFeatureCard/>
-            <AboutFeatureCard/>
-            <AboutFeatureCard/>
-            <AboutFeatureCard/>
-            <AboutFeatureCard/>
+          <div className="flex flex-wrap gap-6">
+            {featureData.map((feature, index) => {
+              return (
+                <div key={index}>
+                  <AboutFeatureCard feature={feature} />
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </section>
+
+      {/* Performance Section  */}
+      <section>
+        <div className="max-w-[1341px] mx-auto mt-[74px]">
+          <div className="flex items-center gap-x-[30px]">
+            <img src={AboutPerformance} alt="" />
+            <div className="">
+              <h5 className='mb-[18px] font-bold text-2xl leading-[29px] text-[#B6B6B6]'>Our performance</h5>
+              <h2 className='mb-[41px] font-bold text-5xl leading-[58px]'>Your Partner for e-
+                commerce grocery
+                solution</h2>
+              <p className='mb-[35px] font-lato text-[#7E7E7E] leading-6 text-justify'>Ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                inventore veritatis et quasi architecto</p>
+              <p className='font-lato text-[#7E7E7E] leading-6 text-justify'>Pitatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+                ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia</p>
+            </div>
+          </div>
+          <div className="my-[50px] flex justify-between">
+            <div className="px-3 py-1">
+              <h3 className='font-bold text-[32px] leading-[38px] mb-[31px]'>Who we are</h3>
+              <p className='max-w-[394px] text-justify font-lato text-[#7E7E7E] leading-6'>Volutpat diam ut venenatis tellus in metus. Nec dui nunc
+                mattis enim ut tellus eros donec ac odio orci ultrices in.
+                ellus eros donec ac odio orci ultrices in.</p>
+            </div>
+            <div className="px-3 py-1">
+              <h3 className='font-bold text-[32px] leading-[38px] mb-[31px]'>Who we are</h3>
+              <p className='max-w-[394px] text-justify font-lato text-[#7E7E7E] leading-6'>Volutpat diam ut venenatis tellus in metus. Nec dui nunc
+                mattis enim ut tellus eros donec ac odio orci ultrices in.
+                ellus eros donec ac odio orci ultrices in.</p>
+            </div>
+            <div className="px-3 py-1">
+              <h3 className='font-bold text-[32px] leading-[38px] mb-[31px]'>Who we are</h3>
+              <p className='max-w-[394px] text-justify font-lato text-[#7E7E7E] leading-6'>Volutpat diam ut venenatis tellus in metus. Nec dui nunc
+                mattis enim ut tellus eros donec ac odio orci ultrices in.
+                ellus eros donec ac odio orci ultrices in.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Counter Section  */}
+      <section>
+        <div className="max-w-[1610px] mx-auto relative rounded-[15px] overflow-hidden">
+          <div className="w-full">
+            <img src={AboutCounter} alt="" className='w-full' />
+          </div>
+          <div className="bg-[#4b675acc] py-[100px] absolute left-0 top-0 w-full">
+            <div className="flex">
+              <div className="w-[320px] h-[115px] text-center font-bold text-white">
+                <h2 className='text-7xl leading-[87px]'>10+</h2>
+                <span className='text-2xl leading-7'>Glorious years</span>
+              </div>
+              <div className="w-[320px] h-[115px] text-center font-bold text-white">
+                <h2 className='text-7xl leading-[87px]'>5000+</h2>
+                <span className='text-2xl leading-7'>Happy clients</span>
+              </div>
+              <div className="w-[320px] h-[115px] text-center font-bold text-white">
+                <h2 className='text-7xl leading-[87px]'>100+</h2>
+                <span className='text-2xl leading-7'>Projects complete</span>
+              </div>
+              <div className="w-[320px] h-[115px] text-center font-bold text-white">
+                <h2 className='text-7xl leading-[87px]'>60+</h2>
+                <span className='text-2xl leading-7'>Team advisor</span>
+              </div>
+              <div className="w-[320px] h-[115px] text-center font-bold text-white">
+                <h2 className='text-7xl leading-[87px]'>100+</h2>
+                <span className='text-2xl leading-7'>Products Sale</span>
+              </div>
+            </div>
           </div>
         </div>
       </section>
