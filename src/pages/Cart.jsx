8d59c444f890a96fa3d6c0bb9ac60ea869a1cb12
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-03-03 04:24:40 +0600

    Cart Page Added

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,22 @@
+import Breadcrumb from "../components/Breadcrumb"
+
+
+const Cart = () => {
+    return (
+        <>
+            <section>
+                <div>
+                    <Breadcrumb />
+                </div>
+
+
+                <div className="max-w-[1610px] mx-auto">
+
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default Cart
